42eddef695f1852a20d8e60844e57c9c9879b7d4
bertyn99 <bboulikou@gmail.com>
2023-11-10 16:24:32 +0100

    Fix typo in app.ts and update dev script in package.json

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app: express.Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Bonjour le monde avec Express et TypeScript!');
+  res.send('Bonjour le mondedd avec Express et TypeScript!');
 });
 
 const PORT: number | string = process.env.PORT || 3000;
