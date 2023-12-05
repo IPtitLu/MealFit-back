aad14443d30761be3766b6fba12c240fe32c5b16
bertyn99 <bboulikou@gmail.com>
2023-12-05 18:01:07 +0100

    Refactor user service methods to include return types

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,17 +2,17 @@ import User from '../models/User';
 import bcrypt from 'bcrypt';
 
 export class UserService {
-    async createUser(userData: any) {
+    async createUser(userData: any): Promise<any> {
         const newUser = await User.create(userData);
         return newUser;
     }
 
-    async getUserById(id: string) {
+    async getUserById(id: string): Promise<any> {
         const user = await User.findOne({ identifiantUtilisateur: id });
         return user;
     }
 
-    async updateUser(id: string, updateData: any) {
+    async updateUser(id: string, updateData: any): Promise<any> {
         if (updateData.hashMotDePasse) {
             updateData.hashMotDePasse = await bcrypt.hash(updateData.hashMotDePasse, 12);
         }
@@ -20,7 +20,7 @@ export class UserService {
         return updatedUser;
     }
 
-    async deleteUser(id: string) {
+    async deleteUser(id: string): Promise<any> {
         const deletedUser = await User.findOneAndDelete({ identifiantUtilisateur: id });
         return deletedUser;
     }
