13a66fc8d126bc4589f0f45ac87fe16130f74829
bertyn99 <bboulikou@gmail.com>
2023-12-06 23:59:06 +0100

    Add morgan for request logging and enable CORS

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,24 @@ import userRoutes from './routes/user.route';
 import groceryRoutes from './routes/grocery.route';
 import recipeRoutes from './routes/recipe.route';
 import cors from 'cors';
-
+import morgan from 'morgan';
 dotenv.config()
 
 const app: Application = express();
+
+
 app.use(express.json());
+
+// log requests
+app.use(morgan('dev'));
+
+// enable cors
 app.use(cors());
+
+// define a route handler for the health check route
 app.route('/').get((req, res) => res.send('Meal fit Express + TypeScript Server'));
+
+// routes
 app.use('/api', userRoutes);
 app.use('/api/groceries', groceryRoutes);
 app.use('/api/recipes', recipeRoutes);
